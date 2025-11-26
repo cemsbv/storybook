ac856e31d4c5ee0d6c069f3b5c27b919d6237986
Khoa Nguyen <khoa.h.nguyen@tuni.fi>
2025-11-26 16:18:44 +0100

    feat button: implement button component

---
+++ src/lib/components/Button/Button.stories.svelte
@@ -0,0 +1,36 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Button from './Button.svelte';
+	import { fn } from 'storybook/test';
+
+	const { Story } = defineMeta({
+		title: 'Components/Button',
+		component: Button,
+		tags: ['autodocs'],
+		argTypes: {
+			size: {
+				control: { type: 'radio' },
+				options: ['sm', 'md', 'lg']
+			},
+			variant: {
+				control: { type: 'radio' },
+				options: ['primary', 'secondary', 'destructive', 'confirmative']
+			},
+			content: { control: 'text' }
+		},
+		args: {
+			onclick: fn()
+		}
+	});
+</script>
+
+<!-- More on writing stories with args: https://storybook.js.org/docs/writing-stories/args -->
+<Story name="Primary" args={{ size: 'md', variant: 'primary' }}>
+	<!-- <Button {...args} /> -->
+</Story>
+
+<!-- <Story name="Secondary" args={{ label: 'Button' }} /> -->
+
+<!-- <Story name="Large" args={{ size: 'large', label: 'Button' }} /> -->
+
+<!-- <Story name="Small" args={{ size: 'small', label: 'Button' }} /> -->
---
+++ src/lib/components/Button/Button.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { type Snippet } from 'svelte';
+
+	export interface ButtonProps {
+		variant: 'primary' | 'secondary' | 'destructive' | 'confirmative';
+		size: 'sm' | 'md' | 'lg';
+		children: Snippet;
+		onclick?: () => void;
+	}
+	let { variant, size, children, onclick }: ButtonProps = $props();
+
+	// Style the button based on the variant and size props
+	let style = $state('rounded-sm text-black font-medium');
+	if (variant === 'primary') {
+		style += ' bg-white border border-solid border-border ';
+	} else if (variant === 'secondary') {
+		style += ' bg-white';
+	} else if (variant === 'destructive') {
+		style += ' text-brand-secondary border border-solid border-brand-secondary';
+	} else if (variant === 'confirmative') {
+		style += ' bg-brand-primary text-white border-none';
+	}
+
+	if (size === 'sm') {
+		style += ' px-md py-sm text-sm';
+	} else if (size === 'md') {
+		style += ' px-lg py-sm text-sm';
+	} else if (size === 'lg') {
+		style += ' px-2xl py-md text-md';
+	}
+</script>
+
+<button class={style} {onclick}>
+	{@render children()}
+</button>
