aa543a33d4f2cf98499525add15899a2d2b184e3
Khoa Nguyen <khoa.h.nguyen@tuni.fi>
2025-11-27 11:10:14 +0100

    fix button: us disabled as a property instead of class for styling

--- src/lib/components/Button/Button.svelte
+++ src/lib/components/Button/Button.svelte
@@ -8,10 +8,10 @@
 		children: Snippet;
 		onclick?: () => void;
 	}
-	let { variant, size, disabled, children, onclick }: ButtonProps = $props();
+	const { variant, size, disabled, children, onclick }: ButtonProps = $props();
 
 	// Style the button based on the variant and size props
-	let buttonClass = $state('rounded-sm text-black font-medium');
+	let buttonClass = $state('rounded-sm text-black font-md');
 	if (variant === 'primary') {
 		buttonClass += ' primary bg-white border border-solid border-border ';
 	} else if (variant === 'secondary') {
@@ -33,53 +33,53 @@
 	}
 </script>
 
-<button class={buttonClass} {onclick}>
+<button class={buttonClass} {onclick} {disabled}>
 	{@render children()}
 </button>
 
-<style lang="css">
+<style lang="scss">
 	button {
 		&.primary {
-			&.disabled {
+			&:disabled {
 				border-color: var(--color-bg-disabled);
 				color: var(--color-bg-disabled);
 				cursor: not-allowed;
 			}
-			&:hover:not(.disabled) {
+			&:hover:not(:disabled) {
 				border-color: var(--color-spacer);
 				color: var(--color-hint);
 				cursor: pointer;
 			}
 		}
 		&.secondary {
-			&.disabled {
+			&:disabled {
 				color: var(--color-bg-disabled);
 				cursor: not-allowed;
 			}
-			&:hover:not(.disabled) {
+			&:hover:not(:disabled) {
 				color: var(--color-hint);
 				cursor: pointer;
 				text-decoration: underline;
 			}
 		}
 		&.destructive {
-			&.disabled {
+			&:disabled {
 				border-color: var(--color-bg-disabled);
 				color: var(--color-bg-disabled);
 				cursor: not-allowed;
 			}
-			&:hover:not(.disabled) {
+			&:hover:not(:disabled) {
 				border-color: var(--color-brand-secondary-hover);
 				color: var(--color-brand-secondary-hover);
 				cursor: pointer;
 			}
 		}
 		&.confirmative {
-			&.disabled {
+			&:disabled {
 				background-color: var(--color-bg-disabled);
 				cursor: not-allowed;
 			}
-			&:hover:not(.disabled) {
+			&:hover:not(:disabled) {
 				background-color: var(--color-brand-primary-hover);
 				cursor: pointer;
 			}
